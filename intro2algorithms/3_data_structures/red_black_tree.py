a9eaf1d63c1f915cd41785ec2927c717fb7e3e6c
William Cheng <shihuac2@illinois.edu>
2023-01-11 11:39:58 -0600

    add insert

--- intro2algorithms/3_data_structures/red_black_tree.py
+++ intro2algorithms/3_data_structures/red_black_tree.py
@@ -16,7 +16,7 @@ RED, BLACK = 0, 1
 """
 
 
-class TreeNode():
+class TreeNode:
     def __init__(self, color=None, key=None):
         self.color = color
         self.key = key
@@ -25,25 +25,76 @@ class TreeNode():
         self.p = None
 
 
-class RBTree():
+NIL = TreeNode(color=BLACK)  # sentinel at the top and bottom of the tree
+
+
+class RBTree:
     def __init__(self):
-        self.root = None
+        self.root = NIL
 
     def insert(self, key):
-        if self.root is None:
-            self.root = TreeNode(color=BLACK, key=key)  # property 2: root color is BLACK
-            return
+        z = TreeNode(color=RED, key=key)    # z is the node to be inserted, color must be RED
+        y = NIL                             # y is the parent of z
+        x = self.root
+        while x != NIL:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z.p = y
+        if y == NIL:
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+        z.left = NIL
+        z.right = NIL
 
-    """ helper functions """
+        # above is normal BST insertion, need to recolor the tree to maintain RB properties
+        while z.p.color == RED:
+            # z.p.p must exist because z.p is RED meaning that z.p is
+            # not the root
+            if z.p == z.p.p.left:
+                y = z.p.p.right
+                if y.color == RED:              # case 1
+                    z.p.color = BLACK
+                    y.color = BLACK
+                    z.p.p.color = RED
+                    z = z.p.p
+                else:
+                    if z == z.p.right:          # case 2
+                        z = z.p
+                        self.left_rotate(z)
+                    z.p.color = BLACK           # case 3
+                    z.p.p.color = RED
+                    self.right_rotate(z.p.p)
+            else:
+                y = z.p.p.left
+                if y.color == RED:              # case 1
+                    z.p.color = BLACK
+                    y.color = BLACK
+                    z.p.p.color = RED
+                    z = z.p.p
+                else:
+                    if z == z.p.left:           # case 2
+                        z = z.p
+                        self.right_rotate(z)
+                    z.p.color = BLACK           # case 3
+                    z.p.p.color = RED
+                    self.left_rotate(z.p.p)
+
+        self.root.color = BLACK
 
     def left_rotate(self, x):
         """ do a left rotation at node x """
         y = x.right                 # set y
         x.right = y.left            # turn y's subtree into x's right subtree
-        if y.left is not None:
+        if y.left != NIL:
             y.left.p = x
         y.p = x.p                   # link x's parent to y
-        if x.p is None:
+        if x.p == NIL:
             self.root = y
         elif x == x.p.left:
             x.p.left = y
@@ -56,16 +107,42 @@ class RBTree():
         """ do a right rotation at node x """
         y = x.left                  # set y
         x.left = y.right            # turn y's subtree into x's left subtree
-        if y.right is not None:
+        if y.right != NIL:
             y.right.p = x
         y.p = x.p                   # link x's parent to y
-        if x.p is None:
+        if x.p == NIL:
             self.root = y
         elif x == x.p.left:
             x.p.left = y
         else:
             x.p.right = y
-        y.right = x                  # put x on y's right
+        y.right = x                 # put x on y's right
         x.p = y
 
 
+def print_tree(root):
+    """ visualize a red-black tree """
+    if root == NIL:
+        return
+    print("current node:", root.key, "\tcolor:", "BLACK" if root.color == 1 else "RED  "
+          , "\tp:", root.p.key, "\tleft:", root.left.key, "\tright:", root.right.key)
+    print_tree(root.left)
+    print_tree(root.right)
+
+
+def main():
+    tree = RBTree()
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(5)
+    tree.insert(7)
+    tree.insert(8)
+    tree.insert(11)
+    tree.insert(14)
+    tree.insert(15)
+    print_tree(tree.root)
+
+
+if __name__ == '__main__':
+    main()
