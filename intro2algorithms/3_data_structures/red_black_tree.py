828ad00b87396f3703d375eb6e16ada62c0f00f4
williamcheng <csh1234_1@126.com>
2023-01-12 10:34:10 -0600

    added some comments

--- intro2algorithms/3_data_structures/red_black_tree.py
+++ intro2algorithms/3_data_structures/red_black_tree.py
@@ -182,42 +182,42 @@ class RBTree:
         while x != self.root and x.color == BLACK:
             if x == x.p.left:
                 w = x.p.right
-                if w.color == RED:
+                if w.color == RED:                                      # case 1
                     w.color = BLACK
                     x.p.color = RED
                     self.left_rotate(x.p)
                     w = x.p.right
-                if w.left.color == BLACK and w.right.color == BLACK:
+                if w.left.color == BLACK and w.right.color == BLACK:    # case 2
                     w.color = RED
                     x = x.p
                 else:
-                    if w.right.color == BLACK:
+                    if w.right.color == BLACK:                          # case 3
                         w.left.color = BLACK
                         w.color = RED
                         self.right_rotate(w)
                         w = x.p.right
-                    w.color = x.p.color
+                    w.color = x.p.color                                 # case 4
                     x.p.color = BLACK
                     w.right.color = BLACK
                     self.left_rotate(x.p)
                     x = self.root
             else:
                 w = x.p.left
-                if w.color == RED:
+                if w.color == RED:                                      # case 1
                     w.color = BLACK
                     x.p.color = RED
                     self.right_rotate(x.p)
                     w = x.p.left
-                if w.right.color == BLACK and w.left.color == BLACK:
+                if w.right.color == BLACK and w.left.color == BLACK:    # case 2
                     w.color = RED
                     x = x.p
                 else:
-                    if w.left.color == BLACK:
+                    if w.left.color == BLACK:                           # case 3
                         w.right.color = BLACK
                         w.color = RED
                         self.left_rotate(w)
                         w = x.p.left
-                    w.color = x.p.color
+                    w.color = x.p.color                                 # case 4
                     x.p.color = BLACK
                     w.left.color = BLACK
                     self.right_rotate(x.p)
